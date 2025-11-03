abe5e2652d92647926758a6fe421569b75668a8f
nolanrso <nolan.rou.22.09@gmail.com>
2025-11-03 17:41:40 +0100

    coms

--- connexion.php
+++ connexion.php
@@ -1,10 +1,16 @@
-
 <?php
-// Récupération des données envoyées par le formulaire
-$identifiant = $_REQUEST['identifiant'];
-$motdepasse = $_REQUEST['motdepasse'];
-
-// Affichage du message
-echo "<h1>Bonjour " . htmlspecialchars($identifiant) . " !</h1>";
-echo "<p>Votre mot de passe est : " . htmlspecialchars($motdepasse) . "</p>";
+include("entete.php")
+?>            
+        <!-- partie principale -->
+        <section>
+            <h2>Titre de section</h2>
+         </section>
+        
+<?php
+include("pieddepage.php")
+?>
+<?php
+$prenom = $_REQUEST["Identifiant"];
+$nom = $_REQUEST["Mot de passe"];
+echo "<p>Bonjour ", $prenom, " ", $nom , "</p>\n";
 ?>
